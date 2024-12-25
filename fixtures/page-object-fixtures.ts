98799d420396a0286d0ced2ce2d0f925e8f82bf9
TechGlobalSchool <tgschool01@gmail.com>
2024-12-24 20:48:41 -0600

    DB Automation with UI

--- fixtures/page-object-fixtures.ts
+++ fixtures/page-object-fixtures.ts
@@ -3,33 +3,38 @@ import { TodoPage } from "../pages/TodoPage";
 import { LoginPage } from "../pages/LoginPage";
 import { BasePage } from "../pages/BasePage";
 import { WaitsPage } from "../pages/WaitsPage";
+import { BackendPage } from "../pages/BackendPage";
 
-type MyFixtures = { 
-  basePage: BasePage
-  todoPage: TodoPage,
-  loginPage: LoginPage
-  waitsPage: WaitsPage
+type MyFixtures = {
+  basePage: BasePage;
+  todoPage: TodoPage;
+  loginPage: LoginPage;
+  waitsPage: WaitsPage;
+  backendPage: BackendPage;
 };
 
 // Extend the Playwright test runner to create your custom fixtures
 export const test = base.extend<MyFixtures>({
+  basePage: async ({ page }, use) => {
+    const basePage = new BasePage(page);
+    await use(basePage);
+  },
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    await use(loginPage);
+  },
+  todoPage: async ({ page }, use) => {
+    const todoPage = new TodoPage(page);
+    await use(todoPage);
+  },
+  waitsPage: async ({ page }, use) => {
+    const waitsPage = new WaitsPage(page);
+    await use(waitsPage);
+  },
+  backendPage: async ({ page }, use) => {
+    const backendPage = new BackendPage(page);
+    await use(backendPage);
+  },
+});
 
-  basePage: async({page}, use) => {
-    const basePage = new BasePage(page)
-    await use(basePage)
-  },
-  loginPage: async({page}, use) => {
-    const loginPage = new LoginPage(page)
-    await use(loginPage)
-  },
-  todoPage: async({page}, use) => {
-    const todoPage = new TodoPage(page)
-    await use(todoPage)
-  },
-  waitsPage: async({page}, use) => {
-    const waitsPage = new WaitsPage(page)
-    await use(waitsPage)
-  },
-})
-
-export { expect } from '@playwright/test'
+export { expect } from "@playwright/test";
